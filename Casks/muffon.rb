f17163d9ec62265b0da10c376f4511be85a39933
k13-za <kieron@kudala.io>
2025-06-20 00:45:28 +0200

    fixed url

--- Casks/muffon.rb
+++ Casks/muffon.rb
@@ -14,12 +14,12 @@ cask "muffon" do
 
   on_arm do
     sha256 "07e19ac200a52be219ced7e0fc7ecf2ab89aa5afef95708433b48e0286aa690f"
-    url "https://github.com/staniel359/muffon/releases/download/v#{version}/Muffon-#{version}-arm64.dmg"
+    url "https://github.com/staniel359/muffon/releases/download/v#{version}/muffon-#{version}-mac-arm64.dmg"
   end
 
   on_intel do
     sha256 "997b1b48ea32089ecc92ab2020f79193285e815c680c8b35f9994d6dc1477ec5"
-    url "https://github.com/staniel359/muffon/releases/download/v#{version}/Muffon-#{version}-x64.dmg"
+    url "https://github.com/staniel359/muffon/releases/download/v#{version}/muffon-#{version}-mac-x64.dmg"
   end
 
   app "muffon.app"
